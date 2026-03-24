048a436d0ec78e1be07897ee4c4bc1c2fe3829b3
ppreciousatim <pprecous@gmail.com>
2026-03-24 20:06:02 +0300

    add js files

---
+++ index.js
@@ -0,0 +1,17 @@
+// variables
+//how can u store variables
+//declaring a variable using keywords var,let,const
+//var is function-scoped & can be declared 
+var name="Precious";
+console.log(name)
+name=Precious
+//let is block-scoped& can be updated but not declared
+let age=20
+console.log(age)
+age=21
+console.log(myagehaschangedbecauseiamagirl)
+
+//const is block-scoped & can not be updated or decalred
+const country=uganda
+console.log(country)
+country=uganda 
